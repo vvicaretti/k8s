2cac93c164b02c18c4267b8819ef8d158876350d
Vincenzo Vicaretti <vvicaretti@zendesk.com>
2018-03-29 12:49:26 +0200

    Download flannel

--- LFS258/bootstrap.sh
+++ LFS258/bootstrap.sh
@@ -1,14 +1,14 @@
 # upgrade
+export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get upgrade -y
 # install requirements
 apt-get install -y apt-transport-https
 # setup k8s repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
 apt-get update
+# install k8s components/utils
 apt-get install -y kubelet kubeadm kubectl
 # download flannel
-# wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
+wget -q -O kube-flannel.yml \
+    https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
