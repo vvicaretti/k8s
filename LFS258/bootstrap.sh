0f04e3f99829c91097c8f48b98c611e4f7bc8b6a
Vincenzo Vicaretti <vvicaretti@zendesk.com>
2018-03-30 15:20:09 +0200

    add tools

--- LFS258/bootstrap.sh
+++ LFS258/bootstrap.sh
@@ -1,14 +1,31 @@
 # upgrade
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get upgrade -y
-# install requirements
-apt-get install -y apt-transport-https
-# setup k8s repository
+
+# setup additional repository
+curl -s https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public | apt-key add -
+curl -s -o /etc/apt/sources.list.d/draios.list https://s3.amazonaws.com/download.draios.com/stable/deb/draios.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
-apt-get update
-# install k8s components/utils
-apt-get install -y kubelet kubeadm kubectl
+
+apt-get update && apt-get upgrade -y
+# install requirements
+apt-get install -y \
+  apt-transport-https \
+  htop \
+  linux-headers-"$(uname -r)" \
+  kubelet \
+  kubeadm \
+  kubectl \
+  sysdig
+
 # download flannel
 wget -q -O kube-flannel.yml \
-    https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+  https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+# enable auto-completion
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+# stern
+wget -q -O /usr/bin/stern \
+  https://github.com/wercker/stern/releases/download/1.6.0/stern_linux_amd64
+chmod +x /usr/bin/stern
